79912f2ca64bea6490b74a2620359f629b76fcc8
egorovin <egorovin@mail.ru>
2023-06-07 00:15:39 +0300

    lesson8

--- app/src/main/java/com/girogevoro/dictionary/view/history/TranslationDialogFragment.kt
+++ app/src/main/java/com/girogevoro/dictionary/view/history/TranslationDialogFragment.kt
@@ -1,11 +1,19 @@
 package com.girogevoro.dictionary.view.history
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.animation.addListener
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.DialogFragment
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import coil.load
 import coil.size.OriginalSize
 import com.girogevoro.dictionary.databinding.FragmentTranslationDialogBinding
@@ -13,7 +21,7 @@ import com.girogevoro.dictionary.databinding.FragmentTranslationDialogBinding
 class TranslationDialogFragment(
     private val header: String,
     private val translation: String,
-    private val imageLink: String
+    private val imageLink: String,
 ) : DialogFragment() {
 
     private var _viewBinding: FragmentTranslationDialogBinding? = null
@@ -25,7 +33,7 @@ class TranslationDialogFragment(
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return FragmentTranslationDialogBinding.inflate(inflater, container, false).also {
             _viewBinding = it
@@ -37,12 +45,32 @@ class TranslationDialogFragment(
             tvHeader.text = header
             tvTranslation.text = translation
             useCoilToLoadPhoto(iv, imageLink)
+
         }
     }
 
     private fun useCoilToLoadPhoto(iv: ImageView, imageLink: String) {
         iv.load("https:$imageLink") {
             size(OriginalSize)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                ValueAnimator.ofFloat(
+                    10f,
+                    1f
+                ).apply {
+                    duration = 3000
+                    interpolator = FastOutLinearInInterpolator()
+                    addUpdateListener {
+                        val value = it.animatedValue as Float
+                        val blurEffect = RenderEffect.createBlurEffect(value, value, Shader.TileMode.DECAL)
+                        iv.setRenderEffect(blurEffect)
+                    }
+                }.start()
+            }
+
+
+
         }
     }
+
 }
